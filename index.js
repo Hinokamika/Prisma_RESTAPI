84160b38e7b98699ef221916ab21bab8914e49f6
Hinokamika <manh.ndm2506@gmail.com>
2025-06-04 01:29:22 +0700

    5

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use('/user', userAuthRoutes);
 app.use('/user-detail', userDetailRoutes);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
     }
 );
