6aebbc7e55816dd76fed93302b4c17e4574e1df8
Hinokamika <manh.ndm2506@gmail.com>
2025-06-03 20:35:19 +0700

    update

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 import express from 'express';
 import userAuthRoutes from './routes/userAuthRoute.js';
 import userDetailRoutes from './routes/userDetailRoute.js';
-import userExerciseRoutes from './routes/userExerciseRoute.js';
-import userHealthRoutes from './routes/userHealthRoute.js';
-import userNutritionRoutes from './routes/userNutritionRoute.js';
 
 const app = express();
 app.use(express.json());
 
 app.use('/user', userAuthRoutes);
 app.use('/user-detail', userDetailRoutes);
-app.use('/exercise-log', userExerciseRoutes);
-app.use('/health-log', userHealthRoutes);
-app.use('/nutrition-log', userNutritionRoutes);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
